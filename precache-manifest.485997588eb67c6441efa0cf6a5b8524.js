21b2ce5d2381456a0985c7cb4cfb58abaa3bdb12
vedant0712 <vedant0712@gmail.com>
2019-11-09 17:13:08 +0530

    Updates

---
+++ precache-manifest.485997588eb67c6441efa0cf6a5b8524.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5384688f382395d2ccf607f3030b654",
+    "url": "/Movie-Rental/index.html"
+  },
+  {
+    "revision": "96e11a4222d2306f773f",
+    "url": "/Movie-Rental/static/css/2.07a9e034.chunk.css"
+  },
+  {
+    "revision": "a7415f84873080e75820",
+    "url": "/Movie-Rental/static/css/main.18c07094.chunk.css"
+  },
+  {
+    "revision": "96e11a4222d2306f773f",
+    "url": "/Movie-Rental/static/js/2.caa612ac.chunk.js"
+  },
+  {
+    "revision": "a7415f84873080e75820",
+    "url": "/Movie-Rental/static/js/main.af459f09.chunk.js"
+  },
+  {
+    "revision": "cbd7d68fc0ac69966fe4",
+    "url": "/Movie-Rental/static/js/runtime-main.d0257050.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Movie-Rental/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Movie-Rental/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Movie-Rental/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Movie-Rental/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Movie-Rental/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
