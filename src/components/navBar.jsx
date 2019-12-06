2514d4b8dc4f8846f3f06c73513fd9465917fb3f
Vedant Choudhary <vedantchoudhary@Vedants-MacBook-Pro.local>
2019-12-06 11:29:12 +0530

    Adding Smartech Scripts New 3

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -5,7 +5,7 @@ const NavBar = ({user}) => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 	  		<Link className="navbar-brand" to="/">
-	  		Vidly
+	  		Vidly Movie
 	  		</Link>	
 	  		<button 
 	  			className="navbar-toggler" 
